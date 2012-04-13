864fcb67c2e5aaf1e1c4d3aef0ef9ae9c81ead2e
davidebbo <david.ebbo@microsoft.com>
2012-04-12 17:31:04 -0700

    StandardHtmlUpload

---
+++ TestUploadApplication/StandardHtmlUpload.aspx
@@ -0,0 +1,17 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="StandardHtmlUpload.aspx.cs" Inherits="TestUploadApplication.StandardHtmlUpload" %>
+
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" enctype="multipart/form-data" method="post">
+    <div>
+        <input id="File1" type="file" name="foo" />
+        <input type="submit" value="Click to upload" />
+    </div>
+    </form>
+</body>
+</html>
---
+++ TestUploadApplication/StandardHtmlUpload.aspx.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Diagnostics;
+
+namespace TestUploadApplication
+{
+    public partial class StandardHtmlUpload : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (Request.Files.Count > 0)
+            {
+                var sw = Stopwatch.StartNew();
+                Request.Files[0].SaveAs(MapPath("~/testupload.bin"));
+                sw.Stop();
+                Response.Write("Upload took " + sw.Elapsed.TotalSeconds);
+            }
+        }
+    }
+}
